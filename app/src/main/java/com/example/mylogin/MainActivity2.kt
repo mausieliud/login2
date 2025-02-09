3f7a3efee8a15d5ed74476f659da37b722c134e0
EliudM <141761979+mausieliud@users.noreply.github.com>
2025-02-09 12:19:37 +0300

    added a second page

--- app/src/main/java/com/example/mylogin/MainActivity2.kt
+++ app/src/main/java/com/example/mylogin/MainActivity2.kt
@@ -1,31 +1,16 @@
 package com.example.mylogin
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        val loginButton=findViewById<Button>(R.id.login_btn)
-            loginButton.setOnClickListener { val intent= Intent(this,MainActivity2::class.java)
-            startActivity(intent)
-            }
-
-
-
-
-
-
+        setContentView(R.layout.activity_main2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
